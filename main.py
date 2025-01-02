dd976e3c86b6f8e8c865a3ed61917dc15d8985a8
Liudmila <lyudatest@mail.ru>
2025-01-02 16:25:10 +0100

    Added function to generate symbols

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ symbols_list = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 ##TODO: Get password rules from user
 print("Welcome to the Meaningful Password Generator!")
-password_type = input("What type of password do you want to create? words or phrase\n")
+password_type = input("What type of password do you want to create? words or phrase\n").lower()
 words_amount = int(input("How many words you want in your password?\n"))
 add_numbers = bool(input("Add numbers: True or False\n").upper())
 numbers_amount = int(input("How many numbers you want in your password?\n"))
 add_symbols = bool(input("Add special characters: True or False\n").upper())
-symbols_amount = int(input("How many numbers you want in your password?\n"))
+symbols_amount = int(input("How many symbols you want in your password?\n"))
 #replace_letters = bool(input("Would you like to replace some common letters with special characters?\n").upper())
 
 #TODO: Get information from the website (when user clicks "Generate password")
@@ -34,11 +34,19 @@ def create_phrase_password(words_amount):
     return phrase_password
 
 
-def add_numbers_to_password(generated_password, numbers_amount):
+def add_numbers_to_password(password, numbers_amount):
     for n in range(numbers_amount):
         number = str(random.randint(0,9))
-        generated_password = generated_password + number
-    return generated_password
+        password += number
+    return password
+
+
+def add_symbols_to_password(password, symbols_list, symbols_amount):
+    for n in range(symbols_amount):
+        random_symbol = random.choice(symbols_list)
+        password += random_symbol
+        print(generated_password)
+    return password
 
 
 def show_password(password):
@@ -54,9 +62,7 @@ if add_numbers:
     generated_password = add_numbers_to_password(generated_password, numbers_amount)
 
 if add_symbols:
-    generated_password = add_special_symbols(generated_password, symbols_list, symbols_amount)
-#    # Generate string with n amount of special symbols according to "symbols_amount) input.
-#    print("Add symbols")
+    generated_password = add_symbols_to_password(generated_password, symbols_list, symbols_amount)
 
 show_password(generated_password)
 
